30d9e06fcef833a1d4f6ffdb803c850e214ca263
José Augusto <josemoura212@gmail.com>
2024-04-20 16:13:38 -0300

    Adicionar funcionalidade de divisão na função print_menu

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ fn timer(time: u64) {
     sleep(Duration::from_secs(time));
 }
 
+fn divider() {
+    println!("{}", "-".repeat(40));
+}
+
 fn print_menu() {
     let options = vec![
         "Escolha uma opção:",
@@ -18,84 +22,112 @@ fn print_menu() {
         "4 - Listar alunos cadastrados",
         "5 - Sair",
     ];
-    println!("---------------------------------");
+    divider();
     for i in &options {
         println!("{}", i);
     }
-    println!("---------------------------------");
+    divider();
 }
 
-fn list_studants(studants: &Vec<(String,String,Vec<(String,f32)>)>) {
-    println!(
-        "---------------------------------\nListando alunos\n---------------------------------"
-    );
+fn list_studants(studants: &Vec<(String, String, Vec<(String, f32)>)>) {
+    clear_screen();
     timer(1);
+    if studants.is_empty() {
+        println!("Não há alunos cadastrados");
+        let mut input = String::new();
+        println!("Pressione enter para continuar");
+        std::io::stdin().read_line(&mut input).unwrap();
+        return;
+    }
     println!("Alunos cadastrados:");
     for i in studants.iter() {
+        divider();
         println!("Nome: {}", i.0);
         println!("Matrícula: {}", i.1);
         println!("Notas:");
         for j in i.2.iter() {
             println!("      {} : {}", j.0,j.1);
         }
-        println!("---------------------------------");
+        divider();
+        let mut input = String::new();
+        println!("Pressione enter para continuar");
+        std::io::stdin().read_line(&mut input).unwrap();
+        clear_screen();
     }
-
-    timer(10);
 }
 
-fn register_studant(studants: &mut Vec<(String,String,Vec<(String,f32)>)>) {
-    println!("---------------------------------\nIniciando cadastro de aluno\n---------------------------------");
-    timer(1);
+fn register_grades(grades: &mut Vec<(String, f32)>) {
+    clear_screen();
+    let mut discipline = String::new();
+    println!("Digite a disciplina do aluno:");
+    std::io::stdin()
+        .read_line(&mut discipline)
+        .expect("Erro ao ler a disciplina");
+
+    discipline = discipline.trim().to_string();
+
+    let mut grade = String::new();
+    println!("\nDigite as notas do aluno:");
+    std::io::stdin()
+        .read_line(&mut grade)
+        .expect("Erro ao ler as notas");
+
+    let grade: f32 = match grade.trim().parse() {
+        Ok(grade) => grade,
+        Err(_) => {
+            println!("Nota inválida");
+            return register_grades(grades);
+        }
+    };
+
+    grades.push((discipline, grade));
+
+    clear_screen();
+
+    let mut input = String::new();
+    println!("Deseja adicionar mais notas? (s/n)");
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Erro ao ler a opção");
+
+    if input.trim() == "n" {
+        return;
+    }
+
+    return register_grades(grades);
+}
+
+fn register_studant(studants: &mut Vec<(String, String, Vec<(String, f32)>)>) {
+    clear_screen();
 
     let mut name = String::new();
-    println!("\nDigite o nome do aluno:");
-    std::io::stdin().read_line(&mut name).expect("Erro ao ler o nome");
+    println!("Digite o nome do aluno:");
+    std::io::stdin()
+        .read_line(&mut name)
+        .expect("Erro ao ler o nome");
+
+    name = name.trim().to_string();
 
     let mut registration = String::new();
     println!("\nDigite a matrícula do aluno:");
-    std::io::stdin().read_line(&mut registration).expect("Erro ao ler a matrícula");
+    std::io::stdin()
+        .read_line(&mut registration)
+        .expect("Erro ao ler a matrícula");
 
-    let mut grades: Vec<(String,f32)> = Vec::new();
+    registration = registration.trim().to_string();
 
-    loop {
-        let mut discipline = String::new();
-        println!("\nDigite a disciplina do aluno:");
-        std::io::stdin().read_line(&mut discipline).expect("Erro ao ler a disciplina");
+    let mut grades: Vec<(String, f32)> = Vec::new();
 
-        let mut grade = String::new();
-        println!("\nDigite as notas do aluno:");
-        std::io::stdin().read_line(&mut grade).expect("Erro ao ler as notas");
+    register_grades(&mut grades);
 
-        let grade: f32 = match grade.trim().parse() {
-            Ok(grade) => grade,
-            Err(_) => {
-                println!("Nota inválida");
-                continue;
-            }
-        };
-
-        grades.push((discipline.trim().to_string(), grade));
-
-        clear_screen();
-
-        let mut input = String::new();
-        println!("Deseja adicionar mais notas? (s/n)");
-        std::io::stdin().read_line(&mut input).expect("Erro ao ler a opção");
-
-        if input.trim() == "n" {
-            break;
-        }
-    }
-
-    let studant = (name.trim().to_string(), registration.trim().to_string(), grades);
+    let studant = (name, registration, grades);
     studants.push(studant);
 }
 
 fn main() {
     println!("Iniciando sistema");
 
-    let mut studants: Vec<(String,String,Vec<(String,f32)>)> = Vec::new();
+    let mut studants: Vec<(String, String, Vec<(String, f32)>)> = Vec::new();
 
     loop {
         print_menu();
